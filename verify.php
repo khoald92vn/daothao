5742a40a1866a0e2fc01475f315d9d9c54bfe040
khoald92vn <khoald92vn@gmail.com>
2024-06-06 21:52:51 +0900

    Update verify.php

--- verify.php
+++ verify.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['pin'])) {
     $pin = $_POST['pin'];
     if ($pin === $correctPin) {
         $_SESSION['authenticated'] = true;
-        header('Location: watch.php');
+        header('Location: watch1.php');
         exit();
     } else {
         echo "<script>alert('Sai rồi hớ nì.'); window.location.href='index.html';</script>";
