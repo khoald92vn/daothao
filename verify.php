b5d27fbb5163b4e3ddcbd5febd9cc8ae2f530408
khoald92vn <khoald92vn@gmail.com>
2024-06-06 21:34:30 +0900

    Add files via upload

--- verify.php
+++ verify.php
@@ -7,7 +7,7 @@ if (isset($_POST['pin'])) {
     $pin = $_POST['pin'];
     if ($pin === $correctPin) {
         $_SESSION['authenticated'] = true;
-        header('Location: video.php');
+        header('Location: watch.php');
         exit();
     } else {
         echo "<script>alert('Sai rồi hớ nì.'); window.location.href='index.html';</script>";
